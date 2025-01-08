f8a6d82977727ba7e56ea10901b83f049c08bfc2
admin_kouadioj <admin_kouadioj@ad.ifsttar.fr>
2025-01-08 13:59:59 +0100

    Dalas => Dallas

--- src/weather_dashboard.py
+++ src/weather_dashboard.py
@@ -73,7 +73,7 @@ def main():
     # Create bucket if needed
     dashboard.create_bucket_if_not_exists()
     
-    cities = ["Philadelphia", "Seattle", "New York","Paris","Abidjan","Cotonou","Dalas"]
+    cities = ["Philadelphia", "Seattle", "New York","Paris","Abidjan","Cotonou","Dallas"]
     
     for city in cities:
         print(f"\nFetching weather for {city}...")
